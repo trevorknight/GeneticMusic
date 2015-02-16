fed4165b628aa896ea79054d73ac902025f02653
Trevor Knight <trevorknight@gmail.com>
2015-02-15 23:09:26 -0800

    Changed MIDI output to using mido and rtmidi, added music21 for music models

--- MidiOutput.py
+++ MidiOutput.py
@@ -1,19 +1,25 @@
 __author__ = 'TrevorKnight'
 
+import mido
 from time import sleep
-import rtmidi
 
 class MidiOutput:
     def __init__(self):
-        self.midi_output = rtmidi.MidiOut();
-        self.output_port = self.midi_output.open_virtual_port("GeneticMusicOutVirtualPort")
+        self.port = None
 
+    def __enter__(self):
+        print('enter')
+        mido.set_backend('mido.backends.rtmidi')
+        self.port = mido.open_output("GeneticMusicOutVirtualPort", virtual=True)
+        sleep(1)  # Ensure the virtual MIDI port has time to register
+        return self
+
+    def __exit__(self, type, value, traceback):
+        sleep(3)  # Wait for note off messages just in case
+        print('exit')
+        del self.port
 
     def play_note(self):
-        note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
-        note_off = [0x80, 60, 0]
-        self.midi_output.send_message(note_on)
-        sleep(0.5)
-        self.midi_output.send_message(note_off)
-
-        del self.midi_output
+        print('play note')
+        message = mido.Message('note_on', note=100, velocity=127, time=6.2)
+        self.port.send(message)
--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 ###Install dependencies
 
+1. http://xquartz.macosforge.org/
+2. http://www.finalemusic.com/products/finale-notepad/resources/
+3. 
+
 ```
-$ pip install -r requirements.txt
+brew install hg
+brew install sdl sdl_image sdl_mixer sdl_ttf portmidi
+(optional)(virtualenv -p [interpreter choice] venv)
+(optional)(source venv/bin/activate)
+pip install hg+http://bitbucket.org/pygame/pygame
+pip install music21
 ```
+
